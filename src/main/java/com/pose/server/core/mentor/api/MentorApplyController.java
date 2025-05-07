d1c330bacf31a899e35053725bf9bd543cdcda29
1eehaewon <lhw2112@nate.com>
2025-05-08 00:22:26 +0900

    refactor : MentorApplyController RestController->Controller 수정

--- src/main/java/com/pose/server/core/mentor/api/MentorApplyController.java
+++ src/main/java/com/pose/server/core/mentor/api/MentorApplyController.java
@@ -5,9 +5,10 @@ import com.pose.server.core.mentor.payload.MentorApplyDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("api/mentor/apply")
 @RequiredArgsConstructor
 public class MentorApplyController {
