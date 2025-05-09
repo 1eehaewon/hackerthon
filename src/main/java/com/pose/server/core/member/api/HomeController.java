53dc8f6d860ccb6e2d2bc728f3acb9ff847d23dc
HakPyun <kjiz0503@gmail.com>
2025-05-09 11:37:44 +0900

    fix : 컨트롤러 동작 수정

--- src/main/java/com/pose/server/core/member/api/HomeController.java
+++ src/main/java/com/pose/server/core/member/api/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -14,13 +15,19 @@ public class HomeController {
     }
 
     @GetMapping("/loginhome")
-    public String loginhome(HttpSession session, Model model) {
+    public String loginhome(HttpSession session, Model model, RedirectAttributes redirectAttributes) {
         String userId = (String) session.getAttribute("user");
         String role = session.getAttribute("role").toString();
 
         if (userId != null) {
             model.addAttribute("user", userId); // 세션에 있는 사용자 정보 전달
             model.addAttribute("role", role);
+        }
+
+        if (userId == null) {
+            // FlashAttribute에 메시지 저장 → 로그인 페이지에서 alert로 처리
+            redirectAttributes.addFlashAttribute("alert", "로그인이 필요합니다.");
+            return "redirect:/members/login";
         }
 
         return "indexG";
